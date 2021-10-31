4b033139f287421443a6911efd1301786f37f0b0
BrandonJodha <b.jodha11@gmail.com>
2021-10-31 17:54:25 -0400

    Deployment pt.2

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  // vue.config.js file to be place in the root of your repository
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/crypto/'
+  : '/'
 }
